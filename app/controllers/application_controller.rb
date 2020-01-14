30e83c1f7f44afdd3ead85a9b5257edb423de141
Gregory Losco <gtlosco@gmail.com>
2020-01-14 02:43:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   end 
   
   delete 'articles/:id' do 
-     Article.destroy(params[:id])
+     Article.delete(params[:id])
      redirect to '/articles'
   end 
   
